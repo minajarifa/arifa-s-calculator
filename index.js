c19016f80f7c492fbb4485943f1f8d5fa992c2d3
Arifa Islam <minajmiha02@gmail.com>
2024-03-06 12:15:55 +0600

    create a calculator by me

---
+++ index.js
@@ -0,0 +1,17 @@
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+  }
+
+  function clearResult() {
+    document.getElementById('result').value = '';
+  }
+
+  function calculate() {
+    let result = document.getElementById('result').value;
+    try {
+      let finalResult = eval(result);
+      document.getElementById('result').value = finalResult;
+    } catch (error) {
+      alert('Invalid input!');
+    }
+  }
